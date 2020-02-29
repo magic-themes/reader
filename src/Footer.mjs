658d8edeed2fd208b822400e81965148448264b2
jascha <jascha@jaeh.at>
2020-02-29 01:49:48 +0100

    add src and test

---
+++ src/Footer.mjs
@@ -0,0 +1,90 @@
+export const View = (state, children) => {
+  const { one, two, three } = state.footer
+
+  return footer({ class: 'Footer' }, [
+    div({ class: 'Container' }, [
+      (one || two || three) &&
+        div({ class: 'Menus' }, [
+          one &&
+            div({ class: 'Child Info' }, [
+              one.title && h2(one.title),
+              one.before && one.before.map(a => p(a)),
+              one.menu && ul([one.menu.map(item => li(Link(item)))]),
+              one.after && one.after.map(a => p(a)),
+            ]),
+          two &&
+            div({ class: 'Child' }, [
+              two.title && h2(two.title),
+              two.before && two.before.map(a => p(a)),
+              two.menu && ul([two.menu.map(item => li(Link(item)))]),
+              two.after && two.after.map(a => p(a)),
+            ]),
+          three &&
+            div({ class: 'Child' }, [
+              three.title && h2(three.title),
+              three.before && three.before.map(a => p(a)),
+              three.menu && ul([three.menu.map(item => li(Link(item)))]),
+              three.after && three.after.map(a => p(a)),
+            ]),
+        ]),
+
+      Credits(state),
+      children,
+    ]),
+  ])
+}
+
+export const style = vars => ({
+  backgroundColor: vars.primary.neutral,
+  color: vars.text.dark,
+  display: 'inline-block',
+  margin: '5% 0 0',
+  padding: 0,
+  width: '100%',
+
+  h2: {
+    margin: 0,
+    color: vars.background.dark,
+  },
+
+  a: {
+    color: vars.text.dark,
+
+    '&:hover': {
+      color: vars.link.hover.light,
+    },
+  },
+
+  '.Container': {
+    margin: '0 auto',
+    maxWidth: vars.maxWidth,
+    textAlign: 'left',
+    padding: '0 5%',
+  },
+
+  '.Menus': {
+    display: 'inline-block',
+    width: '100%',
+    textAlign: 'center',
+  },
+
+  '.Child': {
+    textAlign: 'left',
+
+    ul: {
+      li: {
+        float: 'none',
+      },
+    },
+  },
+
+  [`@media screen and (min-width: ${vars.widths.tablet})`]: {
+    fontSize: '0.9em',
+
+    '.Child': {
+      float: 'left',
+      width: '30%',
+      margin: '0 3% 0 0',
+    },
+  },
+})
---
+++ src/Header.mjs
@@ -0,0 +1,82 @@
+export const View = (props = {}, children = []) => {
+  CHECK_PROPS(props, propTypes, 'Header')
+
+  const { logo, menu, title, ...state } = props
+  const { root } = state
+
+  if (!logo && !menu && !logotext) {
+    return
+  }
+
+  let { branding } = state
+  if (!Array.isArray(branding)) {
+    branding = [branding]
+  }
+
+  const [first, second, ...rest] = branding
+
+  return header({ class: 'Header' }, [
+    div({ class: 'BrandContainer' }, [
+      div([
+        Logo({ root }),
+        branding && Link({ to: root, class: 'branding' }, [first, span(second), rest]),
+      ]),
+    ]),
+    div({ class: 'Toc' }, [
+      Burger(),
+      h3({ title: 'Table of Contents' }, Link({ to: root }, 'ToC')),
+      Menu({ state, items: menu, collapse: false }),
+    ]),
+  ])
+}
+
+export const style = vars => ({
+  padding: '.5em 0 0',
+  width: '100%',
+
+  '.BrandContainer': {
+    width: '100%',
+    textAlign: 'center',
+
+    '> div': {
+      display: 'inline-block',
+    },
+  },
+
+  'a, .Menu li a': {
+    color: vars.white,
+
+    '&:hover': {
+      color: vars.link.hover.dark,
+    },
+  },
+
+  '.branding': {
+    color: vars.neutral,
+    float: 'left',
+    fontSize: '1.5em',
+    lineHeight: '1',
+    margin: '0.4em 0 0 0.2em',
+    textDecoration: 'none',
+
+    span: {
+      color: vars.primary.neutral,
+      fontSize: '1em',
+      margin: '0',
+    },
+  },
+
+  '> div': {
+    margin: '0 auto',
+    maxWidth: '1000px',
+    padding: '0 5%',
+  },
+})
+
+export const propTypes = {
+  Header: [
+    { key: 'logo', type: 'string' },
+    { key: 'logotext', type: 'string' },
+    { key: 'menu', type: 'array' },
+  ],
+}
